68bc6e4721c4b1a0f29c4bdde2b82ef1fc8e4284
Paul4936 <paul.soloveychik@gmail.com>
2026-01-03 01:46:39 -0500

    Complete server.py, webapp ready

---
+++ server.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("FinalProject")
+
+@app.route("/emotionDetector")
+def emotion_detector_route():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    response = list(response.items())
+    emotions = ", ".join([F"{k}: {v}" for k,v in response[:-1]])
+    dominant_emotion = response[-1][1]
+    return F"For the given statement, the system response is {emotions}. The dominant emotion is {dominant_emotion}."
+
+
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+
+    #This functions executes the flask app and deploys it on localhost:5000
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
